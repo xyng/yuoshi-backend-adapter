ec4deb54eb9ac8e8c096bd57cdb23c6861816d5a
Daniel Melchior <daniel.melchior@interweber.de>
2020-04-11 08:29:11 +0200

    chore: refactor tag task model for simpler usage

--- packages/backend-adapter/src/adapters/Tasks/Tag.ts
+++ packages/backend-adapter/src/adapters/Tasks/Tag.ts
@@ -1,32 +1,55 @@
-import AsyncIterableWrapper from "../../helpers/AsyncIterableWrapper"
+import AsyncIterableWrapper, { AsyncSingleInterface } from "../../helpers/AsyncIterableWrapper"
 import { NSTaskContentAdapter } from "../AbstractTaskContentAdapter"
 
 import { AsyncBaseTask, StaticBaseTask } from "./BaseTask"
 import { NSUserTaskSolution } from "../AbstractUserTaskSolutionAdapter"
+import parseContent from "../../helpers/parseContent"
 
 interface TagContent {
-	id: string,
+	id: string
+	category_id: string
+	content_id: string
 	tag: string
 }
 
-export class StaticTag extends StaticBaseTask<TagContent[]> {
+interface ContentType {
+	image: string
+	content: string
+}
+
+export class StaticTag extends StaticBaseTask<{
+	tags: TagContent[],
+	contents: ContentType[]
+}> {
 	public readonly isTraining: boolean = false
 	public readonly type: string = "tag"
-	public contents: TagContent[]
+	public tags: TagContent[]
+	public contents: ContentType[]
 
-	protected init(contents: TagContent[]): void {
-		this.contents = contents
+	protected init(contents: {
+		tags: TagContent[],
+		contents: ContentType[]
+	}): void {
+		this.tags = contents.tags
+		this.contents = contents.contents
 	}
 
-	protected getContents(): TagContent[] {
-		return this.contents;
+	protected getContents(): {
+		tags: TagContent[],
+		contents: ContentType[]
+	} {
+		return {
+			tags: this.tags,
+			contents: this.contents,
+		};
 	}
 }
 
 export class Tag extends AsyncBaseTask<StaticTag> {
 	public readonly type: string = "tag"
 	public readonly isTraining: boolean = false
-	public contents: AsyncIterableWrapper<TagContent>
+	public tags: AsyncIterableWrapper<TagContent>
+	public contents: AsyncSingleInterface<ContentType[]>
 
 	protected init(contents: AsyncIterableWrapper<NSTaskContentAdapter.TaskContent>): void {
 		async function* from() {
@@ -44,26 +67,38 @@ export class Tag extends AsyncBaseTask<StaticTag> {
 			}
 		}
 
-		this.contents = AsyncIterableWrapper.fromAsyncIterable(from())
+		this.tags = AsyncIterableWrapper.fromAsyncIterable(from())
+
+		this.contents = contents
+			.reduceLazy((acc, value) => {
+				return acc + value.content
+			}, "")
+			.then((content) => {
+				return parseContent(content, "--").map((match) => {
+					return {
+						image: match.id,
+						content: match.content
+					}
+				})
+			})
 	}
 
 	async getStatic(): Promise<StaticTag> {
 		return new StaticTag({
 			...this,
-			contents: await this.contents.toArray(),
+			contents: {
+				tags: await this.tags.toArray(),
+				contents: await this.contents.value(),
+			}
 		});
 	}
 
-	createAnswer(answers: {
-		content_id: string,
-		category_id: string,
-		answer_id: string,
-	}[]): NSUserTaskSolution.UserTaskSolutionModel {
+	createAnswer(answers: TagContent[]): NSUserTaskSolution.UserTaskSolutionModel {
 		return this.createSolutionFromContentAnswers(answers.map((answer) => {
 			return {
 				content_id: answer.content_id,
 				quest_id: answer.category_id,
-				answer_id: answer.answer_id
+				answer_id: answer.id
 			}
 		}))
 	}
